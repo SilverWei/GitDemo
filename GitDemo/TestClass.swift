22f5eed90f3a65c9952ab6192aa9b52d0c0b6ae7
silverwei <pcbeta@SilverWeideMac.local>
2015-04-11 15:25:29 +0800

    添加新文件

---
+++ GitDemo/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  GitDemo
+//
+//  Created by silverwei on 15-4-11.
+//  Copyright (c) 2015年 silverwei. All rights reserved.
+//
+
+import Foundation
+
+class Test: NSObject {
+    var name:String
+    var age:Int
+}
